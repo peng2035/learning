8198acbf4821498c2c8b1fd22cb2dd2d4d203754
bobby.peng <466356228@qq.com>
2017-05-31 10:19:34 +0800

    fix string utils add final learning

--- src/main/java/com/bobby/peng/learning/basic/utils/StringUtils.java
+++ src/main/java/com/bobby/peng/learning/basic/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package com.bobby.peng.learning.basic.utils;
 
+import java.lang.reflect.Field;
+
 /**
  *
  *
@@ -17,6 +19,31 @@ public class StringUtils extends org.apache.commons.lang.StringUtils{
         String[] result = str.split(",");
 
         return result;
+    }
+
+    public static String fieldToString(Object object) {
+        Class tClass = object.getClass();
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            while(tClass != null) {
+                Field[] fields = tClass.getDeclaredFields();
+
+                for(Field field : fields) {
+                    sb.append(" , \"").append(field.getName()).append("\" : \"").append(field.get(object)).append("\"");
+                }
+
+                tClass = tClass.getSuperclass();
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(sb.length() > 0) {
+            return sb.substring(2,sb.length());
+        }
+
+        return sb.toString();
     }
 
 }
---
+++ src/main/java/com/bobby/peng/learning/knowledge/point/FinalLearning.java
@@ -0,0 +1,10 @@
+package com.bobby.peng.learning.knowledge.point;
+
+/**
+ * Created by peng2035 on 2017/5/30.
+ */
+public class FinalLearning {
+
+
+
+}
