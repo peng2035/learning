cabae9df02af7dc608430e9f5539913e3a96a2a1
peng2035 <56680537pe>
2017-10-03 10:17:53 +0800

    add disrputor

--- src/main/java/com/bobby/peng/learning/disruptor/LongEvent.java
+++ src/main/java/com/bobby/peng/learning/disruptor/LongEvent.java
@@ -1,4 +1,11 @@
 package com.bobby.peng.learning.disruptor;
 
 public class LongEvent {
+
+    private long value;
+
+    public void set(long value) {
+        this.value = value;
+    }
+
 }
---
+++ src/main/java/com/bobby/peng/learning/disruptor/LongEventHandler.java
@@ -0,0 +1,11 @@
+package com.bobby.peng.learning.disruptor;
+
+import com.lmax.disruptor.EventHandler;
+
+public class LongEventHandler implements EventHandler<LongEvent> {
+
+    @Override
+    public void onEvent(LongEvent longEvent, long l, boolean b) throws Exception {
+        System.out.println("Event : " + longEvent);
+    }
+}
