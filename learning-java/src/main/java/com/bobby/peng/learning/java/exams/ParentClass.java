b598c5facc475eee261dfac9875fb47cbae40e5f
彭天浩 <peng2035@163.com>
2018-01-02 20:59:00 +0800

    fix

---
+++ learning-java/src/main/java/com/bobby/peng/learning/java/exams/ParentClass.java
@@ -0,0 +1,17 @@
+package com.bobby.peng.learning.java.exams;
+
+/**
+ * Created by bobby.peng on 02/01/2018.
+ */
+public class ParentClass {
+
+    public void doExtend1() {
+        System.out.println("in ParentClass do extend1");
+        doExtend2();
+    }
+
+    public void doExtend2() {
+        System.out.println("in ParentClass do extend2");
+    }
+
+}
