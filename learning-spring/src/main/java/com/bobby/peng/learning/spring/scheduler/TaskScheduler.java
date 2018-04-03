000d50203a7a7f9a5b32d6a061edba4041173fe4
彭天浩 <peng2035@163.com>
2018-04-03 13:08:41 +0800

    fix producer log

--- learning-spring/src/main/java/com/bobby/peng/learning/spring/scheduler/TaskScheduler.java
+++ learning-spring/src/main/java/com/bobby/peng/learning/spring/scheduler/TaskScheduler.java
@@ -18,7 +18,7 @@ public class TaskScheduler {
 
     @Scheduled(cron = "*/1 * * * * ?")
     public void start() {
-        for(int i=0;i<10000;i++) {
+        for(int i=0;i<200;i++) {
             kafkaService.randomProduce("pengtianhao-test");
             kafkaService.randomProduce("pengtianhao-test-2");
         }
