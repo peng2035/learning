ce519d1af862332c5e4bfdf7b45a04ce5efa70d5
彭天浩 <peng2035@163.com>
2018-04-03 13:05:19 +0800

    fix producer log

--- learning-spring/src/main/java/com/bobby/peng/learning/spring/kafka/Listener.java
+++ learning-spring/src/main/java/com/bobby/peng/learning/spring/kafka/Listener.java
@@ -1,6 +1,8 @@
 package com.bobby.peng.learning.spring.kafka;
 
 import com.bobby.peng.learning.java.basic.utils.RedisUtils;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -10,7 +12,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.LongAdder;
 
 
 /**
@@ -22,18 +26,31 @@ public class Listener {
     @Autowired
     private RedisUtils<String,String> redisUtils;
 
-    public static Set<Thread> set = new HashSet<>();
+    public static Map<String,LongAdder> map = Maps.newConcurrentMap();
 
-    @KafkaListener(topics = "pengtianhao-test",groupId = "pengtianhao-test-group-1")
+    private final static String PENGTIANHAO_TEST_GROUP_1 = "pengtianhao-test-group-1";
+
+    private final static String PENGTIANHAO_TEST_GROUP_2 = "pengtianhao-test-group-1";
+
+    @KafkaListener(topics = "pengtianhao-test",groupId = PENGTIANHAO_TEST_GROUP_1)
     public void listen(ConsumerRecord<String, String> cr) {
-        log.info("pengtianhao-test-group-1 : " + cr.toString());
-        redisUtils.set(Thread.currentThread().getName(),cr.value());
-        set.add(Thread.currentThread());
+
+        map.computeIfAbsent(PENGTIANHAO_TEST_GROUP_1,k -> new LongAdder()).increment();
+
+        if((map.get(PENGTIANHAO_TEST_GROUP_1).longValue() & 4095l) == 4095l) {
+            log.info(PENGTIANHAO_TEST_GROUP_1 + " : " + cr.toString());
+        }
+//        redisUtils.set(Thread.currentThread().getName(),cr.value());
+//        set.add(Thread.currentThread());
     }
 
-    @KafkaListener(topics = "pengtianhao-test",groupId = "pengtianhao-test-group-2")
-    public void listen2(String value) {
-        log.info("pengtianhao-test-group-2 : " + value);
+    @KafkaListener(topics = "pengtianhao-test",groupId = PENGTIANHAO_TEST_GROUP_2)
+    public void listen2(ConsumerRecord<String, String> cr) {
+        map.computeIfAbsent("pengtianhao-test-group-2",k -> new LongAdder()).increment();
+
+        if((map.get(PENGTIANHAO_TEST_GROUP_2).longValue() & 4095l) == 4095l) {
+            log.info(PENGTIANHAO_TEST_GROUP_2 + " : " + cr.toString());
+        }
     }
 
 
